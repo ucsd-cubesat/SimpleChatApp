78ca29ff7dc1997936b75521e0336504d8e94d89
d1jun <64993639+d1jun@users.noreply.github.com>
2021-11-20 16:39:18 -0800

    Create table with username and password

---
+++ src/main/java/com/tritoncubed/chatapp/LoginServlet.java
@@ -0,0 +1,82 @@
+package com.tritoncubed.chatapp;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import java.util.HashMap;
+import java.util.Map;
+//import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.tritoncubed.launchapi.exceptions.APIException;
+
+/**
+ * A servlet that performs a small ping for the user. A simple GET request is made, and a simple
+ * acknowledgement is sent back.
+ * 
+ * http://launchapi.tritoncubed.com/ping
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 6659111915305916881L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    /**
+     * This method contains the logic of the ping request. The request itself is contained in its
+     * namesake object, and the response is created in its namesake object.
+     * 
+     * The response is a JSON object with the following layout:
+     * {
+     *     "response": "pong",
+     *     "timestamp": 0123456789
+     * }
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+          String username = request.getParameter();
+    	  String password = request.getParameter();
+    	  
+    	  AmazonDynamoDB DB = AmazonDynamoDBClient.builder()
+                  .withRegion(Regions.US_EAST_2)
+                  .build();
+
+           DynamoDB dynamoDB = new DynamoDB(client);
+
+           Table table = dynamoDB.getTable("UserLogins");
+           
+           final Map<String, Object> infoMap = new HashMap<String, Object>();
+           
+           final Map<String, Object> infoMap = new HashMap<String, Object>();
+           infoMap.put("password", password);
+
+           try {
+               System.out.println("Adding a new item...");
+               PutItemOutcome outcome = table
+            		   .putItem(new Item().withPrimaryKey("username", username).withMap("info", infoMap));
+
+               System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+           }
+           catch (Exception e) {
+               System.err.println("Unable to add item: " + year + " " + title);
+               System.err.println(e.getMessage());
+           }
+           
+    	  response.getWriter().append("Login Successful");
+    }
+}
